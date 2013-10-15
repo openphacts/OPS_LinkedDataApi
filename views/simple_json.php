8aacb8a6c425b727c08883e80a67dc1336a6409d
fundatureanu-sever <fundatureanu.sever@gmail.com>
2013-10-15 18:00:12 +0200

    Updated Gene-Disease API calls according to feedback

--- views/simple_json.php
+++ views/simple_json.php
@@ -1,15 +1,6 @@
 <?php
-switch($this->ConfigGraph->getEndpointType()){
-    case API.'ListEndpoint' : 
-        $pageUri = $this->Request->getUriWithPageParam();
-        break;
-    case API.'ItemEndpoint' :
-    default:
-        $pageUri = $this->Request->getUri();
-        break;
-}
 
-$json = $DataGraph->to_simple_json($pageUri) ;
+$json = $DataGraph->to_simple_json($this->pageUri) ;
 if($callback = $Request->getParam('_callback')){
 #    $callback = preg_replace("/[^_a-zA-Z0-9\.]/", "", $callback);
     echo "{$callback}({$json})"; 
