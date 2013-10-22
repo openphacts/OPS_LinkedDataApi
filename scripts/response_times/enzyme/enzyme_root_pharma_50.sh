ae5038b3a178b25251af55b57782bc63fa1f8776
antonisloizou <a.loizou@vu.nl>
2013-10-22 15:04:25 +0200

    Added preliminary version of response time measuring scripts

---
+++ scripts/response_times/enzyme/enzyme_root_pharma_50.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export TIMEFORMAT='%3R'
+
+for cmpd in $(cat root)
+do
+	time curl -H 'Cache-Control: no-cache' "https://beta.openphacts.org/target/enzyme/pharmacology/pages?app_id=be7ce5a9&app_key=bd7d4c9a98f16fb8f472f507f88b2b74&uri=`echo -ne $cmpd | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g'`&_pageSize=50" 2>/dev/null 
+done
