1d87bf50e297010c82339c59c2e17d86a0f1406b
Antonis Loizou <a.loizou@vu.nl>
2014-09-15 13:41:00 +0200

    Added script for generating usage metrics for the  3scale lua script

---
+++ api-config-files/generate_metrics.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z "$1" -o -z "$2" ]
+then
+echo "Run from the same directory as swagger.json . Usage : ./generate_metrics [version] [3scale-provider-key]"
+exit 1
+fi
+
+if [ -f rules.lua.tmp ]
+then
+rm rules.lua.tmp
+fi
+
+if [ -f curl.tmp ]
+then 
+rm curl.tmp
+fi
+
+counter=1
+for path in `grep ^\"path swagger.json |grep -v \{ | sed -e 's,.* ",,' -e 's,".*,,' -e 's,^,/1.4,'`
+do
+name=`sed -n 's,^ *"summary,,p' swagger.json|sed -e 's,[^"]*",,' -e 's,[^"]*",,' -e 's,\[PREVIEW\] ,,' -e 's,".*,,' | grep -v 'Activity Units for Type' | sed -n "$counter"p | sed -e 's,Information,Info,' -e 's,:,,' -e "s,$, ($1),"`
+id=`echo $name | sed -e 's,.*,\L&,' -e 's,[()],,g' -e 's,[\. ],_,g' -e 's,_\([0-9]_[0-9]\),-\1,'`
+echo 'curl -v  -X POST "https://openphacts-admin.3scale.net/admin/api/services/1006371755042/metrics/2555417663272/methods.xml" -d "provider_key='$2'&friendly_name='$name'&system_name='$id'&unit=hits"' >> curl.tmp
+echo '
+     local m =  ngx.re.match(path,[=[^'`echo $path | sed 's,\.,\\\.,'`']=])
+     if (m and (method == "GET" or method=="POST")) then
+     -- rule: '$path' --
+         table.insert(matched_rules, "'$path'")
+
+         usage_t["'$id'"] = set_or_inc(usage_t, "'$id'", 1)
+         found = true
+     end
+
+' >> rules.lua.tmp
+counter=$((counter+1))
+done
