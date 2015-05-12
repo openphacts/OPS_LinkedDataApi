ad7f300618be1a29d45ff2e1bd6e1a54ddc9cb29
antonisloizou <a.loizou@vu.nl>
2015-05-12 11:28:57 +0200

    Fix for missing single quotes: https://github.com/openphacts/GLOBAL/issues/243

--- data_handler_components/single_expansion_viewer.class.php
+++ data_handler_components/single_expansion_viewer.class.php
@@ -39,6 +39,7 @@ class SingleExpansionViewer implements Viewer {
 		$response = $this->SparqlEndpoint->graph($this->viewQuery, PUELIA_RDF_ACCEPT_MIMES);
 		if($response->is_success()){
 			$rdf = $response->body;
+			$rdf = preg_replace("/&#39;/", "'", $rdf);
 			$rdf = preg_replace("/&#[a-z0-9]*;/", " ", $rdf);
 			if(isset($response->headers['content-type'])){
 				if(strpos($response->headers['content-type'], 'turtle')){
