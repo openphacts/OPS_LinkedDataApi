fadbcfd51f17bf2ed7a3d953d79baf2252cf2f12
Stian Soiland-Reyes <stain@apache.org>
2016-04-25 16:04:09 +0100

    report as 2.1

--- graphs/linkeddataapigraph.class.php
+++ graphs/linkeddataapigraph.class.php
@@ -40,7 +40,7 @@ class LinkedDataApiGraph extends PueliaGraph {
         $this->_current_page_uri = $pageUri;
         $container = array(
             'format' => 'linked-data-api',
-            "version" => "2.0",
+            "version" => "2.1",
             "result" => $this->_resource_to_simple_json_object($pageUri, $pageUri, false, array()),
         );
         $index = $this->get_index();
@@ -268,7 +268,7 @@ class LinkedDataApiGraph extends PueliaGraph {
         $format = $dom->createAttribute('format');
         $format->appendChild($dom->createTextNode('linked-data-api'));
         $version = $dom->createAttribute('version');
-        $version->appendChild($dom->createTextNode('2.0'));
+        $version->appendChild($dom->createTextNode('2.1'));
         $resultEl->appendChild($format);
         $resultEl->appendChild($version);   
 
