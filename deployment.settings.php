a3aaecae8b9493860ca3c0ff30b7e6d34c34dcda
Randy Kerber <randy@randykerber.com>
2017-05-26 21:43:23 -0400

    Set ‘CACHE_OFF’ to 0, thus disable.

--- deployment.settings.php
+++ deployment.settings.php
@@ -8,7 +8,7 @@ define('CACHE_ONE_DAY', (60*60*24*1));
 define('CACHE_ONE_WEEK', (60*60*24*7));
 define('CACHE_ONE_HOUR', (60*60));
 define('CACHE_ONE_YEAR', time() + (60*60*24*7*52));
-define('CACHE_OFF', 0);
+define('CACHE_OFF', 1);
 define('PUELIA_CACHE_AGE', CACHE_ONE_YEAR);
 define('PUELIA_MEMCACHE_HOST', 'localhost');
 define('PUELIA_MEMCACHE_PORT', '11211');
