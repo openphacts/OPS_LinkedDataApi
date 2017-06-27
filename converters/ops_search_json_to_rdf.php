82bd79de6fec3cbe3f13b7a6e9d61b2c00370be7
Ian Dunlop <ian.dunlop@manchester.ac.uk>
2017-06-27 14:48:23 +0100

    Return uri and labels for each hit

--- converters/ops_search_json_to_rdf.php
+++ converters/ops_search_json_to_rdf.php
@@ -1,7 +1,7 @@
 <?php
 //input in $response in json format
 
-require 'converters/conceptwiki_util.inc.php';
+//require 'converters/conceptwiki_util.inc.php';
 
 $paramNameToPredicate = array( 'query' => 'searchTerm',
                                'limit' => 'limit',
@@ -17,11 +17,7 @@ if (empty($decodedResponse)){
     throw new EmptyResponseException("No results returned from the ConceptWiki");
 }
 $resultBNode = '_:searchResult';
-$this->DataGraph->add_resource_triple($resultBNode, OPS_API.'#result', "A result");
 
-//$searchType = getSearchType($this->Request->getPathWithoutExtension());
-//$resultBNode = '_:searchResult';
-//$this->DataGraph->add_literal_triple($resultBNode, RDF_TYPE, $searchType);
 
 $unreservedParameters = $this->Request->getUnreservedParams();
 foreach ($unreservedParameters as $name => $value){
@@ -29,38 +25,26 @@ foreach ($unreservedParameters as $name => $value){
     $this->DataGraph->add_literal_triple($resultBNode, OPS_API.'#'.$predSuffix, $value);
 }
 
-//link the resultBNode with the UUIDs and their tags
 $tagCounter = 0;
 $urlCounter = 0;
 
-// foreach ($decodedResponse as $elem){
-//     $uuidNode = CONCEPTWIKI_PREFIX.$elem->{"uuid"};
-//     $this->DataGraph->add_resource_triple($resultBNode, OPS_API.'#result', $uuidNode);
+foreach ($decodedResponse as $elem){
+    foreach ($elem->{"hits"} as $hit) {
+            $id = $hit->{"_id"};
+	        $source = $hit->{"_source"};
 
-//     foreach ($elem->{"labels"} as $label){
-//         addLabelWithLanguage($uuidNode, $label, $this->DataGraph);
-//     } 
-    
-//     $this->DataGraph->add_literal_triple($uuidNode, OPS_API.'#match', $elem->{'match'});
-    
-//     foreach ($elem->{"tags"} as $tag){
-//         $tagBNode = '_:tagNode'.$tagCounter;
-//         $this->DataGraph->add_resource_triple($uuidNode, OPS_API.'#semanticTag', $tagBNode);
-        
-//         addConceptWithLabels($tagBNode, $tag, $this->DataGraph);
-//         $tagCounter++;
-//     }
-    
-//     if (isset($elem->{"urls"})){
-//     	foreach ($elem->{"urls"} as $url){
-//     		$urlBNode = '_:urlNode'.$urlCounter;
-//     		$this->DataGraph->add_resource_triple($uuidNode, SKOS.'exactMatch', $urlBNode);
-//     		$this->DataGraph->add_resource_triple($urlBNode, CONCEPTWIKI_PREFIX.'#url', $url->{'value'});
-//     		$this->DataGraph->add_resource_triple($urlBNode, CONCEPTWIKI_PREFIX.'#matchType', $url->{'type'});
-//     		$urlCounter++;
-//     	}
-//     }
-// }
+            $urlBNode = '_:urlNode'.$urlCounter;
+            $uuidNode = $id;
+            $this->DataGraph->add_resource_triple($urlBNode, $uuidNode.'#url', $id);
+            foreach ($source->{"label"} as $label) {
+            	$this->DataGraph->add_literal_triple($urlBNode, SKOS.'prefLabel', $label);
+            }
+            $this->DataGraph->add_resource_triple($resultBNode, OPS_API.'#result', $urlBNode);
+            
+            $urlCounter++;
+    }
+}
+
 
 $rdfData = $this->DataGraph->to_ntriples();//assuming nothing else is in the graph
 
@@ -68,19 +52,6 @@ $rdfData = $this->DataGraph->to_ntriples();//assuming nothing else is in the gra
 $this->DataGraph->add_resource_triple($this->pageUri, FOAF.'primaryTopic', $resultBNode);
 $this->DataGraph->add_resource_triple($resultBNode , FOAF.'isPrimaryTopicOf', $this->pageUri);
 $this->DataGraph->add_resource_triple($this->Request->getUri(), API.'definition', $this->endpointUrl);
-
-// function getSearchType($path){
-//     if (endsWith("freetext", $path)){
-//         return "Freetext Search";
-//     }
-//     else if (endsWith("byTag", $path)){
-//         return "Search by tag";
-//     }
-//     else{
-//         throw new ErrorException('Unknown search type');
-//     }
-// }
-
 
 
 ?>
